df68ef2eef770e5fd6ae136c01344ce34b04fd91
Chausseray Christophe <chausseray.christophe@gmail.com>
2016-06-26 20:21:29 +0100

    [Init app] Add the logger module.

---
+++ www/app/blocks/logger/logger.js
@@ -0,0 +1,69 @@
+/**
+ * Logger
+ * @namespace Blocks
+ */
+(function() {
+  'use strict';
+
+  angular
+    .module('blocks.logger')
+    .factory('logger', logger);
+
+  /* @ngInject */
+  logger.$inject = ['$log'];
+
+  /**
+   * @name logger
+   * @desc Define all types of logger.
+   * @memberOf Blocks
+   */
+  function logger($log) {
+    var service = {
+      error: error,
+      info: info,
+      success: success,
+      warning: warning,
+      log: $log.log
+    }
+
+    return service;
+
+    ///////////////
+
+    /**
+     * @name error
+     * @desc Define the error type.
+     * @memberOf Blocks.logger
+     */
+    function error(message) {
+      $log.error('Error : ' + message);
+    }
+
+    /**
+     * @name info
+     * @desc Define the error type.
+     * @memberOf Blocks.logger
+     */
+    function info(message) {
+      $log.info('Info : ' + message);
+    }
+
+    /**
+     * @name success
+     * @desc Define the error type.
+     * @memberOf Blocks.logger
+     */
+    function success(message) {
+      $log.info('Success : ' + message);
+    }
+
+    /**
+     * @name warning
+     * @desc Define the error type.
+     * @memberOf Blocks.logger
+     */
+    function warning(message) {
+      $log.warn('Warning : ' + message);
+    }
+  }
+})();
